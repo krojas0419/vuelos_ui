135eb3d939656e8faf1ec66ae02c28e769ed333a
karen <karenrojas0419@gmail.com>
2020-11-26 19:12:37 -0500

    Internacionalización en dos idiomas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router} from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -9,11 +10,21 @@ import { Router} from '@angular/router';
 })
 export class AppComponent {
   title = 'vuelosUI';
+  langs: string[] =[];
 
-constructor(private router: Router){}
+constructor(private router: Router, private translate: TranslateService){
+  this.translate.setDefaultLang('es');
+  this.translate.use('es');
+  this.translate.addLangs(['es', 'en']);
+  this.langs = this.translate.getLangs();
+}
 
 listar(){
   return this.router.navigate(['listarVuelos']);
+}
+
+cambiarIdioma(lang: string){
+this.translate.use(lang);
 }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListarComponent } from './components/pasajero/listar/listar.component';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 import { PasajeroService } from './services/pasajero/pasajero.service';
-
+import { AppComponent } from './app.component';
+import { ListarComponent } from './components/pasajero/listar/listar.component';
 import { ListarVueloComponent } from './components/vuelo/listar-vuelo/listar-vuelo.component';
 import { CrearComponent } from './components/pasajero/crear/crear.component';
 import { EditarComponent } from './components/pasajero/editar/editar.component';
@@ -28,7 +33,14 @@ import { EditarComponent } from './components/pasajero/editar/editar.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     PasajeroService
